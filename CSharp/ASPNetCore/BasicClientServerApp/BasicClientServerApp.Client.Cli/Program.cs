26424359fe4298cfafb66b58029f5c7e03e9386e
Dmitrij Patuk <dmitrij.patuk@tectiqom.de>
2019-12-29 15:13:56 +0100

    Youtube Video ASP .Net Core 3.1 - Client Code

---
+++ CSharp/ASPNetCore/BasicClientServerApp/BasicClientServerApp.Client.Cli/Program.cs
@@ -0,0 +1,27 @@
+﻿using BasicClientServerApp.Client.Cli.Services;
+using System;
+
+namespace BasicClientServerApp.Client.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Client");
+            var employeeService = new EmployeeService();
+            Console.WriteLine("Press enter to continue");
+            var id = Console.ReadLine();
+            var result = employeeService.GetEmployee(id);
+                if(result.Length > 3)
+                {
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.WriteLine(result);
+                    Console.WriteLine("Retrying...");
+                }
+
+        }
+    }
+}
---
+++ CSharp/ASPNetCore/BasicClientServerApp/BasicClientServerApp.Client.Cli/Services/EmployeeService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace BasicClientServerApp.Client.Cli.Services
+{
+    class EmployeeService
+    {
+
+        private static readonly HttpClient _httpClient = new HttpClient();
+        //private readonly string _baseUri;
+
+        //public EmployeeService(string baseUri)
+        //{
+        //    _baseUri = baseUri;
+        //}
+
+        public string GetEmployee(string id)
+        {
+             var result = _httpClient
+                .GetAsync($"https://localhost:44308/Employee/Find/{id}")
+                .GetAwaiter()
+                .GetResult();
+            if(result.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+               var stringResult = result.Content.ReadAsStringAsync()
+                    .GetAwaiter()
+                    .GetResult();
+                return stringResult;
+            }
+            else
+            {
+                return result.StatusCode.ToString();
+            }
+        }
+
+    }
+}
