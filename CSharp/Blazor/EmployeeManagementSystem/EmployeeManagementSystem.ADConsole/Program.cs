37aefe4282c264ee12220ff1e67129b278e0e5af
Dmitrij Patuk <dmitrij.patuk@tectiqom.de>
2021-02-01 20:29:20 +0100

    Blazor Tutorial Part 6

---
+++ CSharp/Blazor/EmployeeManagementSystem/EmployeeManagementSystem.ADConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using EmployeeManagementSystem.ADLibs;
+using EmployeeManagementSystem.Data.SQLiteLayer;
+using System;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.ADConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await SelectUsers();            
+        }
+
+        public static async Task SelectUsers()
+        {
+            var userManager = new UserManager("AdminUser", "AdminuserPassword", "Domain");
+            var queries = new EmployeeQueries("ConnecitonStringToDB");
+            var e = await queries.SelectAllEmployee();
+            foreach (var employee in e)
+            {
+                var name = await queries.SelectName(new Data.Shared.Entities.Name { Id = employee.NameId });
+                var login = await queries.SelectLogin(new Data.Shared.Entities.Login { Uuid = employee.LoginUuid});
+                userManager.CreateUser("OU=MySubOu,OU=MyMainOu,DC=xyz,DC=local", login.Username, login.Password + login.Salt, name.First, name.Last, employee.Email, employee.Cell);
+            }
+        }
+    }
+}
---
+++ CSharp/Blazor/EmployeeManagementSystem/EmployeeManagementSystem.ADLibs/UserManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
+using System.Text;
+
+namespace EmployeeManagementSystem.ADLibs
+{
+    public class UserManager
+    {
+        private readonly string username;
+        private readonly string password;
+        private readonly string domain;
+
+        public UserManager(string username, string password, string domain)
+        {
+            this.username = username;
+            this.password = password;
+            this.domain = domain;
+        }
+
+        public void CreateUser(string path, string userToCreateName, string userToCreatePassword, string givenName, string surname, string mail, string phone)
+        {
+            var context = new PrincipalContext(ContextType.Domain, domain, path, username, password);
+            var user = new UserPrincipal(context, userToCreateName, userToCreatePassword, true);
+            user.GivenName = givenName;
+            user.Surname = surname;
+            user.EmailAddress = mail;
+            user.VoiceTelephoneNumber = phone;
+            user.Save();
+        }
+    }
+}
