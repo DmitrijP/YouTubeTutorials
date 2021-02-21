4b355aeb55ed028c993a83bb58943b74df1024a9
Dmitrij Patuk <dmitrij.patuk@tectiqom.de>
2021-02-21 09:48:03 +0100

    Yield Return Tutorial

---
+++ CSharp/LINQTutorial/LINQTutorial/Models/EmployeeModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LINQTutorial
+{
+    internal class EmployeeModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public DateTime BirthDay { get; set; }
+        public override string ToString()
+        {
+            return $"{Id} {FirstName} {LastName} {City} {BirthDay}";
+        }
+    }
+}
--- CSharp/LINQTutorial/LINQTutorial/Program.cs
+++ CSharp/LINQTutorial/LINQTutorial/Program.cs
@@ -6,55 +6,28 @@ namespace LINQTutorial
 {
     class Program
     {
+        static readonly string[] FirstNames 
+            = new string[] { "Dmitrij", "Denis", "Paul", "Andrej", "Jack", "Luke" };
+        static readonly string[] LastNames 
+            = new string[] { "Patuk", "Bauer", "Skywalker", "Calrissian", "Muller", "Wolf" };
+        static readonly string[] Cities 
+            = new string[] { "Dresden", "Berlin", "Heidelberg", "Karlsruhe", "Mannheim", "Bruchsal" };
+
         static void Main(string[] args)
         {
-            var employeeArray = GenerateEmployeeArray(30);
+            var employeeArray = GenerateEmployeeArray(300000);
 
-            var linqResult = from x in employeeArray 
-                             where FindUser(x)
-                             orderby x.BirthDay descending
-                             select x;
-            PrintEmployeeArray(linqResult);
+            var result = SearchWithYield(employeeArray);
+            PrintEmployeeArray(result);
 
-            //var result = SearchWithForLoop(employeeArray);
-            //PrintEmployeeArray(result);
-            
             Console.ReadLine();
         }
 
-        private static bool FindUser(EmployeeModel model)
-        {
-            return model.LastName == "Patuk";
-        }
-
-        private static string GenerateNewCityName()
+        private static IEnumerable<EmployeeModel> GenerateEmployeeArray(int count)
         {
             var random = new Random((int)DateTime.Now.Ticks);
-            return Cities[random.Next(0, 5)];
-        }
-
-        private static List<EmployeeModel> SearchWithForLoop(EmployeeModel[] employeeArray)
-        {
-            var result = new List<EmployeeModel>();
-            for (int i = 0; i < employeeArray.Length; i++)
-                if (employeeArray[i].LastName == "Patuk")
-                    result.Add(employeeArray[i]);
-            return result;
-        }
-
-        static void PrintEmployeeArray(IEnumerable<EmployeeModel> a)
-        {
-            foreach (var item in a)
-                Console.WriteLine(item.ToString());
-        }
-
-        static EmployeeModel[] GenerateEmployeeArray(int count)
-        {
-            var random = new Random((int)DateTime.Now.Ticks);
-            var arr = new EmployeeModel[count];
             for (int i = 0; i < count; i++)
-            {
-                arr[i] = new EmployeeModel
+                yield return new EmployeeModel
                 {
                     Id = i,
                     FirstName = FirstNames[random.Next(0, 5)],
@@ -62,25 +35,25 @@ namespace LINQTutorial
                     City = Cities[random.Next(0, 5)],
                     BirthDay = DateTime.Now - TimeSpan.FromDays(365 * random.Next(18, 70))
                 };
-            }
-            return arr;
         }
 
-        static string[] FirstNames = new string[] { "Dmitrij", "Denis", "Paul", "Andrej", "Jack", "Luke" };
-        static string[] LastNames = new string[] { "Patuk", "Bauer", "Skywalker", "Calrissian", "Muller", "Wolf" };
-        static string[] Cities = new string[] { "Dresden", "Berlin", "Heidelberg", "Karlsruhe", "Mannheim", "Bruchsal" };
-    }
-
-    class EmployeeModel
-    {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string City { get; set; }
-        public DateTime BirthDay { get; set; }
-        public override string ToString()
+        private static IEnumerable<EmployeeModel> SearchWithYield(IEnumerable<EmployeeModel> inputList)
         {
-            return $"{Id} {FirstName} {LastName} {City} {BirthDay}";
+            foreach (var item in inputList)
+                if (FindUser(item))
+                    yield return item;
+                
+        }
+
+        private static void PrintEmployeeArray(IEnumerable<EmployeeModel> a)
+        {
+            foreach (var item in a)
+              Console.WriteLine(item.ToString());
+        }
+
+        private static bool FindUser(EmployeeModel model)
+        {
+            return model.LastName == "Patuk";
         }
     }
 }
